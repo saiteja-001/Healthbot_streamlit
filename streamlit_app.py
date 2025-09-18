3e633ab904ab8bf3a5ff9e47a9b1bf377c145797
saiteja-001 <Saitejakgp2019@gmail.com>
2025-09-18 09:03:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 PROJECT_ID = os.environ.get("healthbot-472404")
-GOOGLE_CREDS = os.environ.get(r"C:\Users\KIIT0001\Desktop\HEALTHBOT\healthbot-472404-1569872ea9f4.json")
+GOOGLE_CREDS = os.environ.get("healthbot-472404-1569872ea9f4.json")
 
-os.environ[r"C:\Users\KIIT0001\Desktop\HEALTHBOT\healthbot-472404-1569872ea9f4.json"] = GOOGLE_CREDS
+os.environ["healthbot-472404-1569872ea9f4.json"] = GOOGLE_CREDS
 
 def detect_intent_text(text, session_id="streamlit-session", language_code="en"):
     session_client = dialogflow.SessionsClient()
@@ -39,5 +39,6 @@ if prompt := st.chat_input("Type your health query..."):
 
     st.session_state.messages.append({"role": "assistant", "content": reply})
     st.chat_message("assistant").markdown(reply)
+
 
 
